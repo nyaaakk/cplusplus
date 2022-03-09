fa0c1a9a2f56a7871598c30c9b2117940e86da66
nyaaakk <ryannpicard@gmail.com>
2022-03-09 10:46:47 +0100

    arbrerrr

--- cplusplus/arbre.cpp
+++ cplusplus/arbre.cpp
@@ -1,5 +1,69 @@
 #include "ElCh2.cpp"
 
-class arbre{
+class Arbre{
 
+    //attributs
+    private: ElCh2* pDebut;
+
+    //constructeurs
+    public:
+    Arbre(){
+        pDebut = nullptr;
+    }
+
+    Arbre(ElCh2* element){
+        pDebut = element;
+    }
+
+    //getters - setters
+    public:
+    ElCh2* getPDebut(){
+        return pDebut;
+    }
+
+    void setPDebut(ElCh2* newPDebut){
+        pDebut = newPDebut;
+    }
+
+    //fonctions
+    void ajouterDroite(double newDonnee){
+        ElCh2* memoire = pDebut;
+        ElCh2* element = new ElCh2(newDonnee);
+        if( memoire == nullptr ){
+            pDebut = element;
+        }
+        else{
+            while( memoire->getSuivantDroite() != nullptr ){
+                memoire = memoire->getSuivantDroite();
+            }
+            memoire->setSuivantDroite(element);
+        }
+    }
+
+    void ajouterGauche(double newDonnee){
+        ElCh2* memoire = pDebut;
+        ElCh2* element = new ElCh2(newDonnee);
+        if( memoire == nullptr ){
+            pDebut = element;
+        }
+        else{
+            while( memoire->getSuivantGauche() != nullptr ){
+                memoire = memoire->getSuivantGauche();
+            }
+            memoire->setSuivantGauche(element);
+        }
+    }
+
+    void afficherArbre(){
+        ElCh2* memoire = pDebut;
+        if( memoire == nullptr ){
+            cout << "l arbre est vide" << endl;
+        }
+        else{
+            do{
+                cout << memoire->getDonnee() << endl;
+            }
+            while( memoire->getSuivantDroite() != nullptr );
+        }
+    }
 };
